3f563bf5a7b723a7e51f04a4743f63e6613a2ccf
asifzcpe <rahmanasif05004@gmail.com>
2019-01-01 18:29:21 +0600

    initial commit

---
+++ src/SyncMigServiceProvider.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Asif\SyncMig;
+use Illuminate\Support\ServiceProvider;
+
+class SyncMigServiceProvider extends ServiceProvider
+{
+	public function boot()
+	{
+
+	}
+
+	public function register()
+	{
+
+	}
+}
